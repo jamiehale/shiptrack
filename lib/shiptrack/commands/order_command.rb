df98def6890f916e1259ef76861a478e7c1d8293
Jamie Hale <jamie@smallarmyofnerds.com>
2014-01-31 11:14:16 -0500

    Add OrderCommand spec

--- lib/shiptrack/commands/order_command.rb
+++ lib/shiptrack/commands/order_command.rb
@@ -5,24 +5,31 @@ module ShipTrack
     def initialize
       super( 'order' )
       describe 'Record an order'
-      handle_parameters [ [ :name, :string ] ]
-      handle_option :vendor, { :type => :string }
-      handle_option :date, { :type => :string }
+      handle_parameters [ CommandParameter.new( :name, :string ) ]
+      handle_option CommandOption.new( :vendor, :string )
+      handle_option CommandOption.new( :date, :string )
     end
 
     def run( params, configuration, options )
       shipment = Shipment.new( { :name => params[ :name ] } )
       shipment.vendor = options[ :vendor ] unless options[ :vendor ].nil?
-      if options[ :date ].nil?
-        shipment.order_date = DateTime.now.strftime( '%Y-%m-%d' )
-      else
-        shipment.order_date = options[ :date ]
-      end
-      shipment_list = ShipmentList.load( configuration[ :active_shipments_filepath ] )
-      shipment_list.add( shipment )
-      shipment_list.save( configuration[ :active_shipments_filepath ] )
+      shipment.order_date = order_date( options )
+      add_to_active_shipment_list( shipment, configuration )
     end
+    
+    private
+    
+      def order_date( options )
+        return options[ :date ] unless options[ :date ].nil?
+        DateTime.now.strftime( '%Y-%m-%d' )
+      end
 
+      def add_to_active_shipment_list( shipment, configuration )
+        shipment_list = ShipmentList.load( configuration[ :active_shipments_filepath ] )
+        shipment_list.add( shipment )
+        shipment_list.save( configuration[ :active_shipments_filepath ] )
+      end
+    
   end
 
 end
