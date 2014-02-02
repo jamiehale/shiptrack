d5d096e3e9dc85d09db30100c6f43020e5a6f3b9
Jamie Hale <jamie@smallarmyofnerds.com>
2014-02-01 20:13:04 -0500

    Add ConfigurationFile

---
+++ lib/shiptrack/configuration_file.rb
@@ -0,0 +1,69 @@
+module ShipTrack
+  
+  class ConfigurationFile
+
+    def initialize( items = {} )
+      @items = keys_strings_to_symbols( items )
+    end
+    
+    def empty?
+      @items.empty?
+    end
+    
+    def []( name )
+      @items[ name ]
+    end
+    
+    def []=( name, value )
+      if name.is_a? String
+        @items[ name.to_sym ] = keys_strings_to_symbols( value )
+      else
+        @items[ name ] = keys_strings_to_symbols( value )
+      end
+    end
+    
+    def self.load( filename )
+      ConfigurationFile.new( YAML.load_file( filename ) )
+    end
+    
+    def save( filename )
+      File.open( filename, 'w' ) do |f|
+        f.write( to_yaml )
+      end
+    end
+    
+    def to_yaml
+      key_symbols_to_strings( @items ).to_yaml
+    end
+    
+    private
+    
+      def keys_strings_to_symbols( thing )
+        return thing unless thing.is_a? Hash
+        filtered_items = {}
+        thing.each do |key,value|
+          if key.is_a? String
+            filtered_items[ key.to_sym ] = keys_strings_to_symbols( value )
+          else
+            filtered_items[ key ] = keys_strings_to_symbols( value )
+          end
+        end
+        filtered_items
+      end
+      
+      def key_symbols_to_strings( thing )
+        return thing unless thing.is_a? Hash
+        filtered_items = {}
+        thing.each do |key,value|
+          if key.is_a? Symbol
+            filtered_items[ key.to_s ] = key_symbols_to_strings( value )
+          else
+            filtered_items[ key ] = key_symbols_to_strings( value )
+          end
+        end
+        filtered_items
+      end
+      
+  end
+  
+end
---
+++ spec/shiptrack/configuration_file_spec.rb
@@ -0,0 +1,95 @@
+require 'spec_helper.rb'
+
+module ShipTrack
+  
+  describe ConfigurationFile do
+
+    it 'builds defaults to empty' do
+      configuration_file = ConfigurationFile.new
+      expect( configuration_file ).to be_empty
+    end
+    
+    it 'builds from a hash' do
+      configuration_file = ConfigurationFile.new( { 'path' => 'some/path' } )
+      expect( configuration_file ).to_not be_empty
+    end
+
+    it 'converts string keys to symbols on creation' do
+      configuration_file = ConfigurationFile.new( { 'path' => 'some/path' } )
+      expect( configuration_file[ :path ] ).to eq( 'some/path' )
+    end
+    
+    it 'converts string keys to symbols on setting' do
+      configuration_file = ConfigurationFile.new
+      configuration_file[ 'path' ] = 'some/path'
+      expect( configuration_file[ :path ] ).to eq( 'some/path' )
+    end
+    
+    describe 'loading' do
+
+      let( :yaml ) { {} }
+      let( :configuration_file ) { double( 'configuration_file' ) }
+      
+      before( :each ) do
+        YAML.stub( :load_file ).and_return( yaml )
+        ConfigurationFile.stub( :new ).and_return( configuration_file )
+      end
+      
+      it 'defers to YAML for loading' do
+        YAML.should_receive( :load_file ).with( 'some/file' )
+        ConfigurationFile.load( 'some/file' )
+      end
+      
+      it 'creates a new ConfigurationFile from the YAML' do
+        ConfigurationFile.should_receive( :new ).with( yaml )
+        ConfigurationFile.load( 'some/file' )
+      end
+      
+      it 'returns the new configuration file' do
+        new_configuration_file = ConfigurationFile.load( 'some/file' )
+        expect( new_configuration_file ).to eq( configuration_file )
+      end
+      
+      it 'creates a new file if the passed one does not exist' do
+        #ConfigurationFile.load( 'some/file' )
+      end
+      
+    end
+    
+    describe 'to_yaml' do
+      
+      let( :params ) { { 'path' => 'some/file', 'group' => [ 'first', 'second', 'third' ] } }
+      let( :configuration_file ) { ConfigurationFile.new( params ) }
+      let( :yaml ) { configuration_file.to_yaml }
+      
+      it 'converts key values' do
+        expect( configuration_file.to_yaml ).to eq( params.to_yaml )
+      end
+      
+    end
+    
+    describe 'saving' do
+
+      let( :configuration_file ) { ConfigurationFile.new }
+      let( :file ) { double( 'file' ) }
+      
+      before( :each ) do
+        File.stub( :open ).and_yield( file )
+        file.stub( :write )
+      end
+      
+      it 'opens the file' do
+        File.should_receive( :open ).with( 'some/file', 'w' )
+        configuration_file.save( 'some/file' )
+      end
+      
+      it 'writes the yaml' do
+        file.should_receive( :write ).with( configuration_file.to_yaml )
+        configuration_file.save( 'some/file' )
+      end
+      
+    end
+      
+  end
+  
+end
