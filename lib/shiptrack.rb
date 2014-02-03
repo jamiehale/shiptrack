d18bd4af5e0108e34f3b46985491273133d93951
Jamie Hale <jamie@smallarmyofnerds.com>
2014-02-02 20:28:18 -0500

    Move yaml and fileutils require to lib/shiptrack.rb

--- lib/shiptrack.rb
+++ lib/shiptrack.rb
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with ShipTrack. If not, see <http://www.gnu.org/licenses/>.
 
+require 'yaml'
+require 'fileutils'
+
 require 'shiptrack/command'
 require 'shiptrack/command_processor'
 require 'shiptrack/command_parameter'
